c31867a34d939ec0e78a5a8a16b5935efa09fd27
FoJkee <romanovsky0815@gmail.com>
2023-06-12 12:35:10 +0300

    fix 15

--- src/repositories/blogs-repositories.ts
+++ src/repositories/blogs-repositories.ts
@@ -1,59 +1,59 @@
-import {BlogsType} from "../types/types";
-
-
-export let blogs: BlogsType[] = []
-const date = new Date()
-
-export const repositoryBlogs = {
-
-    async findBlogs(): Promise<BlogsType[]> {
-        return blogs
-    },
-
-    async createBlogs(name: string, description: string, websiteUrl: string, isMembership: boolean): Promise<BlogsType> {
-
-        const blogsPost = {
-            id: (+date).toString(),
-            name: name,
-            description: description,
-            websiteUrl: websiteUrl,
-            createdAt: date.toISOString(),
-            isMembership: false
-        }
-        blogs.push(blogsPost)
-        return blogsPost
-    },
-
-    async findBlogsId(id: string): Promise<BlogsType | null> {
-        let blogsGet = blogs.find(el => el.id === id)
-        if(blogsGet) {
-            return blogsGet
-        } else {
-            return null
-        }
-    },
-
-    async updateBlogs(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
-        const blogsPut = blogs.find(el => el.id === id)
-        if (blogsPut) {
-            blogsPut.name = name
-            blogsPut.description = description
-            blogsPut.websiteUrl = websiteUrl
-            return true
-        } else {
-            return false
-        }
-    },
-    async deleteBlogs(id: string): Promise<BlogsType | boolean | null> {
-        const blog = this.findBlogsId(id)
-        if (!blog) return null
-        blogs = blogs.filter(b => b.id !== id)
-        return true
-
-    },
-    async deleteBlogsAll() {
-        blogs.splice(0)
-    }
-
-}
-
+// import {BlogsType} from "../types/types";
+//
+// type BlogViewType = BlogsType & { id: string }
+// export let blogs: BlogsType[] = []
+// const date = new Date()
+//
+// export const repositoryBlogs = {
+//
+//     async findBlogs(): Promise<BlogsType[]> {
+//         return blogs
+//     },
+//
+//     async createBlogs(name: string, description: string, websiteUrl: string, isMembership: boolean): Promise<BlogsType> {
+//
+//         const blogsPost = {
+//             id: (+date).toString(),
+//             name: name,
+//             description: description,
+//             websiteUrl: websiteUrl,
+//             createdAt: date.toISOString(),
+//             isMembership: false
+//         }
+//         blogs.push(blogsPost)
+//         return blogsPost
+//     },
+//
+//     async findBlogsId(id: string): Promise<BlogsType | null> {
+//         let blogsGet = blogs.find(el => el.id === id)
+//         if(blogsGet) {
+//             return blogsGet
+//         } else {
+//             return null
+//         }
+//     },
+//
+//     async updateBlogs(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
+//         const blogsPut = blogs.find(el => el.id === id)
+//         if (blogsPut) {
+//             blogsPut.name = name
+//             blogsPut.description = description
+//             blogsPut.websiteUrl = websiteUrl
+//             return true
+//         } else {
+//             return false
+//         }
+//     },
+//     async deleteBlogs(id: string): Promise<BlogsType | boolean | null> {
+//         const blog = this.findBlogsId(id)
+//         if (!blog) return null
+//         blogs = blogs.filter(b => b.id !== id)
+//         return true
+//
+//     },
+//     async deleteBlogsAll() {
+//         blogs.splice(0)
+//     }
+//
+// }
+//
--- src/repositories/posts-repositories.ts
+++ src/repositories/posts-repositories.ts
@@ -1,65 +1,65 @@
-import {postsType} from "../types/types";
-
-
-export let posts: postsType[] = []
-const date = new Date()
-export const repositoryPosts = {
-
-    async findPosts(): Promise<postsType[]> {
-        return posts
-    },
-
-    async createPosts(title: string, shortDescription: string,
-                      content: string, blogId: string, blogName: string): Promise<postsType> {
-
-        const postsPost = {
-            id: (+date).toString(),
-            title: title,
-            shortDescription: shortDescription,
-            content: content,
-            blogId: blogId,
-            blogName: blogName,
-            createdAt: date.toISOString()
-        }
-
-        posts.push(postsPost)
-        return postsPost
-
-    },
-
-    async findPostsId(id: string): Promise<postsType | null> {
-        let findGetId = posts.find(el => el.id === id)
-        if (findGetId) {
-            return findGetId
-        } else {
-            return null
-        }
-    },
-
-    async updatePosts(id: string, title: string, shortDescription: string,
-                      content: string, blogId: string): Promise<boolean> {
-        const postsPut = posts.find(el => el.id === id)
-        if (postsPut) {
-            postsPut.title = title
-            postsPut.shortDescription = shortDescription
-            postsPut.content = content
-            postsPut.blogId = blogId
-            return true
-        } else {
-            return false
-        }
-    },
-
-    async deletePosts(id: string): Promise<boolean | null> {
-        const post = this.findPostsId(id)
-        if (!post) return null
-        posts = posts.filter(p => p.id !== id)
-        return true
-
-    },
-    async deletePostsAll() {
-        posts.splice(0)
-    }
-
-}
-
+// import {postsType} from "../types/types";
+//
+//
+// export let posts: postsType[] = []
+// const date = new Date()
+// export const repositoryPosts = {
+//
+//     async findPosts(): Promise<postsType[]> {
+//         return posts
+//     },
+//
+//     async createPosts(title: string, shortDescription: string,
+//                       content: string, blogId: string, blogName: string): Promise<postsType> {
+//
+//         const postsPost = {
+//             id: (+date).toString(),
+//             title: title,
+//             shortDescription: shortDescription,
+//             content: content,
+//             blogId: blogId,
+//             blogName: blogName,
+//             createdAt: date.toISOString()
+//         }
+//
+//         posts.push(postsPost)
+//         return postsPost
+//
+//     },
+//
+//     async findPostsId(id: string): Promise<postsType | null> {
+//         let findGetId = posts.find(el => el.id === id)
+//         if (findGetId) {
+//             return findGetId
+//         } else {
+//             return null
+//         }
+//     },
+//
+//     async updatePosts(id: string, title: string, shortDescription: string,
+//                       content: string, blogId: string): Promise<boolean> {
+//         const postsPut = posts.find(el => el.id === id)
+//         if (postsPut) {
+//             postsPut.title = title
+//             postsPut.shortDescription = shortDescription
+//             postsPut.content = content
+//             postsPut.blogId = blogId
+//             return true
+//         } else {
+//             return false
+//         }
+//     },
+//
+//     async deletePosts(id: string): Promise<boolean | null> {
+//         const post = this.findPostsId(id)
+//         if (!post) return null
+//         posts = posts.filter(p => p.id !== id)
+//         return true
+//
+//     },
+//     async deletePostsAll() {
+//         posts.splice(0)
+//     }
+//
+// }
+//
